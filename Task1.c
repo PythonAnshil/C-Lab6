a569eafbe21ffc2db12fbb93312d57bad3dd587f
@Nny <162193522+PythonAnshil@users.noreply.github.com>
2024-11-11 22:58:19 -0500

    Update Task1.c

--- Task1.c
+++ Task1.c
@@ -1,20 +1,38 @@
 //Write a program using a switch that performs simple arithmetic operations (addition, subtraction, multiplication, division) based on user input (operator +, -, *, /).
 #include <stdio.h>
+
 int main() {
-    double amount, discount = 0.0, finalAmount;
-    printf("Enter the amount spent: ");
-    scanf("%lf", &amount);
-    if (amount >= 100) {
-        discount = amount * 0.10;
-        printf("10%% discount applied: $%.2lf\n", amount * 0.10);
-        if (amount >= 500) {
-            double additionalDiscount = amount * 0.05;
-            discount += additionalDiscount;
-            printf("Additional 5%% discount applied: $%.2lf\n", additionalDiscount);
-        }
+    double num1, num2, result;
+    char operator;
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("The result of addition: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("The result of subtraction: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("The result of multiplication: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("The result of division: %.2lf\n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator. Please enter one of +, -, *, or /.\n");
     }
-    finalAmount = amount - discount;
-    printf("Total discount applied: $%.2lf\n", discount);
-    printf("Final amount to be paid: $%.2lf\n", finalAmount);
     return 0;
 }
